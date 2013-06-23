8ff45b41c635a5649994eb1e2739fd1370bd02b0
Aleksander Podlaski <aleksander.podlaski@gmail.com>
2013-06-23 07:37:09 +0200

    przeniesione view helpery

--- lib/common-rails.rb
+++ lib/common-rails.rb
@@ -1,12 +1,20 @@
 require 'action_controller/base'
+require 'common-rails/controller'
 
 module CommonRails
   class Railtie < ::Rails::Railtie
+    
     config.app_generators do |g|
-    #config.app_generators.scaffold_controller = :nmc_controller
-    #config.app_generators.template_engine :nmc
+      #config.app_generators.scaffold_controller = :nmc_controller
+      #config.app_generators.template_engine :nmc
       g.templates.unshift File::expand_path('../templates', __FILE__)
       g.stylesheets = false
+    end
+    
+    initializer "extend Controller with sorcery" do |app|
+      ActionController::Base.send(:include, CommonRails::Controller)
+      # ActionController::Base.helper_method :current_user
+      # ActionController::Base.helper_method :logged_in?
     end
     
     rake_tasks do
---
+++ lib/common-rails/controller.rb
@@ -0,0 +1,66 @@
+module CommonRails
+  module Controller
+
+    def full_title(page_title)
+      base_title = t("title")
+      if page_title.blank?
+        base_title
+      else
+        "#{base_title} | #{page_title}"
+      end
+    end
+  
+    def flash_to_bootstrap(name)
+      case name
+        when :notice then "success"
+        when :alert then "error"
+        else name
+      end
+    end
+  
+    def form_standard_buttons_tag(f, controller)
+      content_tag :div, class: "form-actions" do
+        concat link_to('Anuluj', controller, class: "btn")
+        concat " "
+        concat f.button(:submit, class: "btn-success")
+      end
+    end
+  
+    def table_standard_buttons_tag(element)
+      content_tag :span, class: "pull-right" do
+        concat link_to('Edytuj', send("edit_#{element.class.name.underscore}_path", element), class: "btn btn-small") if can? :update, element
+        concat " "
+        concat link_to('Usuń', element, method: :delete, data: { confirm: 'Czy jesteś pewien?' }, class: "btn btn-small btn-danger") if can? :destroy, element
+      end
+    end
+  
+    IGNORED_COLUMNS = %w(id created_at updated_at) 
+
+    def table_tag(elements, *fields)
+      return if elements.empty?
+
+      if fields.empty?
+        fields = elements.first.class.column_names.select { |column| IGNORED_COLUMNS.exclude? column }
+      end
+
+      ths = fields.map do |value|
+        content_tag(:th, elements.first.class.human_attribute_name(value))
+      end
+
+      thead = content_tag :thead, content_tag(:tr, ths.reduce(:+))
+
+      trs = elements.map do |element|
+        tds = fields.map do |value|
+          content_tag(:td, element.try(value))
+        end
+      
+        content_tag(:tr, tds.reduce(:+) + content_tag(:td, table_standard_buttons_tag(element)))
+      end
+
+      tbody = content_tag :tbody, trs.reduce(:+)
+
+      content_tag :table, thead.concat(tbody), class: "table"
+    end
+
+  end
+end
