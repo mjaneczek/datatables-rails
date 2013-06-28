a40eadc6fe0d5435575b416958b68daec8400b84
Aleksander Podlaski <aleksander.podlaski@gmail.com>
2013-06-28 10:27:03 +0200

    poprawianie sciezki do templates

--- lib/common-views.rb
+++ lib/common-views.rb
@@ -3,19 +3,19 @@ require 'rails'
 
 module CommonViews
   class Railtie < ::Rails::Railtie
-    
+
     config.app_generators do |g|
       #config.app_generators.scaffold_controller = :nmc_controller
       #config.app_generators.template_engine :nmc
-      g.templates.unshift File::expand_path('../templates', __FILE__)
+      g.templates.unshift File::expand_path('templates', __FILE__)
       g.stylesheets = false
     end
-    
+
     initializer "extend Controller with sorcery" do |app|
       #ActionController::Base.send(:include, CommonRails::Controller)
       #self.responder = CommonRails::ApplicationResponder
     end
-    
+
     rake_tasks do
       load "tasks/db_all.rake"
     end
