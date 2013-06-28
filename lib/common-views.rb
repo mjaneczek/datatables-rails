47d2f766b0a73b846d616ce907b6e7d58bcb873d
Aleksander Podlaski <aleksander.podlaski@gmail.com>
2013-06-28 11:02:03 +0200

    zmuszenie gemu aby odpalał nasz scaffolding

--- lib/common-views.rb
+++ lib/common-views.rb
@@ -5,9 +5,9 @@ module CommonViews
   class Railtie < ::Rails::Railtie
 
     config.app_generators do |g|
-      #config.app_generators.scaffold_controller = :nmc_controller
+      config.app_generators.scaffold_controller = :scaffold_controller
       #config.app_generators.template_engine :nmc
-      g.templates.unshift File::expand_path('./templates', __FILE__)
+      g.templates.unshift File::expand_path('../templates', __FILE__)
       g.stylesheets = false
     end
 
